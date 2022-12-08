25d7f9794e78321f64304e1ba634b8328b50f2c9
tomoro kobori <46224993+kettle-tomorrow@users.noreply.github.com>
2022-12-08 09:39:23 +0900

    test: 正常系と異常系のテストを追加する

--- spec/minesweeper_spec.rb
+++ spec/minesweeper_spec.rb
@@ -1,7 +1,96 @@
+require './minesweeper'
+
 RSpec.describe 'Minesweeper' do
   describe '.start' do
-    it do
-      expect('aa').to eq 'aa'
+    context 'クリアできなかった場合' do
+      it '爆弾があったことを示す文字列が出力され、ゲームが中断される' do
+        allow(Minesweeper).to receive(:rand).and_return(0, 0, 1, 1)
+  
+        allow(ARGF).to receive(:gets).and_return(StringIO.new('1,1').gets)
+  
+        expected_stdout = <<~EOS
+  マインスイーパを開始します。
+  爆弾は全部で2個あります。
+  回答を入力してください。(例: 1,1)
+  ■ ■ ■
+  ■ ■ ■
+  ■ ■ ■
+  爆弾です。
+  マインスイーパを終了します。
+        EOS
+  
+        expect { Minesweeper.start }.to output(expected_stdout).to_stdout
+      end
+    end
+
+    context 'クリアできた場合' do
+      it  'マスがすべて開き、正常にゲームが終了する' do
+        allow(Minesweeper).to receive(:rand).and_return(0, 0, 1, 1)
+  
+        allow(ARGF).to receive(:gets).and_return(
+          StringIO.new('1,2').gets,
+          StringIO.new('1,3').gets,
+          StringIO.new('2,1').gets,
+          StringIO.new('2,3').gets,
+          StringIO.new('3,1').gets,
+          StringIO.new('3,2').gets,
+          StringIO.new('3,3').gets,
+        )
+  
+        expected_stdout = <<~EOS
+  マインスイーパを開始します。
+  爆弾は全部で2個あります。
+  回答を入力してください。(例: 1,1)
+  ■ ■ ■
+  ■ ■ ■
+  ■ ■ ■
+  ---
+  次の回答を入力してください。
+  ---
+  ■ 2 ■
+  ■ ■ ■
+  ■ ■ ■
+  ---
+  次の回答を入力してください。
+  ---
+  ■ 2 1
+  ■ ■ ■
+  ■ ■ ■
+  ---
+  次の回答を入力してください。
+  ---
+  ■ 2 1
+  2 ■ ■
+  ■ ■ ■
+  ---
+  次の回答を入力してください。
+  ---
+  ■ 2 1
+  2 ■ 1
+  ■ ■ ■
+  ---
+  次の回答を入力してください。
+  ---
+  ■ 2 1
+  2 ■ 1
+  1 ■ ■
+  ---
+  次の回答を入力してください。
+  ---
+  ■ 2 1
+  2 ■ 1
+  1 1 ■
+  ---
+  クリアです。
+  ---
+  ✖ 2 1
+  2 ✖ 1
+  1 1 1
+  マインスイーパを終了します。
+        EOS
+  
+        expect { Minesweeper.start }.to output(expected_stdout).to_stdout
+      end
     end
   end
 end
