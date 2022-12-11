1d584b264632622e5ab6a9f5a640e969a1d9d96a
tomoro kobori <46224993+kettle-tomorrow@users.noreply.github.com>
2022-12-11 10:13:48 +0900

    refactor: 盤面のマスと爆弾の数を可変にできるよう、変数に置き換える

--- board.rb
+++ board.rb
@@ -7,50 +7,14 @@ class Board
     row3 = [{ display: "■", arround_mine_number: 0, is_mine: false }, { display: "■", arround_mine_number: 0, is_mine: false }, { display: "■", arround_mine_number: 0, is_mine: false }]
     @rows = [row1, row2, row3]
 
-    mine1 = mines[0]
-    mine2 = mines[1]
+    mines.each { |mine| set_mines(mine) }
 
-    @rows[mine1[0]][mine1[1]][:is_mine] = true
-    @rows[mine2[0]][mine2[1]][:is_mine] = true
+    @row_min = 0
+    @row_max = @rows.size - 1
+    @squire_min = 0
+    @squire_max = @rows.first.size - 1
 
-    (0..2).each do |i|
-      (0..2).each do |j|
-        if @rows[i][j][:is_mine] == false
-          top = i - 1
-          down = i + 1
-          left = j - 1
-          right = j + 1
-          if top >= 0
-            @rows[i][j][:arround_mine_number] += 1 if @rows[top][j][:is_mine] == true
-          end
-          if down <= 2
-             @rows[i][j][:arround_mine_number] += 1 if @rows[down][j][:is_mine] == true
-          end
-          if left >= 0
-            @rows[i][j][:arround_mine_number] += 1 if @rows[i][left][:is_mine] == true
-          end
-          if right <= 2
-            @rows[i][j][:arround_mine_number] += 1 if @rows[i][right][:is_mine] == true
-          end
-          top_left = [top, left]
-          top_right = [top, right]
-          down_left = [down, left]
-          down_right = [down, right]
-          if top_left[0] >= 0 && top_left[1] >= 0
-            @rows[i][j][:arround_mine_number] += 1 if @rows[top_left[0]][top_left[1]][:is_mine] == true
-          end
-          if top_right[0] >= 0 && top_right[1] <= 2
-            @rows[i][j][:arround_mine_number] += 1 if @rows[top_right[0]][top_right[1]][:is_mine] == true
-          end
-          if down_left[0] <= 2 && down_left[1] >= 0
-            @rows[i][j][:arround_mine_number] += 1 if @rows[down_left[0]][down_left[1]][:is_mine] == true
-          end
-          if down_right[0] <= 2 && down_right[1] <= 2
-            @rows[i][j][:arround_mine_number] += 1 if @rows[down_right[0]][down_right[1]][:is_mine] == true
-          end
-        end
-      end
-    end
+    set_arround_mine_numbers
   end
 
   def current_board
@@ -75,5 +39,52 @@ class Board
 
   def completed_board
     @rows.map { |row| row.map { |squire| squire[:is_mine] == true ? "✖" : squire[:display] }.join(" ") }
+  end
+
+  private
+
+  def set_mines(mine)
+    @rows[mine[0]][mine[1]][:is_mine] = true
+  end
+
+  def set_arround_mine_numbers
+    (@row_min..@row_max).each do |i|
+      (@squire_min..@squire_max).each do |j|
+        if @rows[i][j][:is_mine] == false
+          top = i - 1
+          down = i + 1
+          left = j - 1
+          right = j + 1
+          if top >= @row_min
+            @rows[i][j][:arround_mine_number] += 1 if @rows[top][j][:is_mine] == true
+          end
+          if down <= @row_max
+             @rows[i][j][:arround_mine_number] += 1 if @rows[down][j][:is_mine] == true
+          end
+          if left >= @squire_min
+            @rows[i][j][:arround_mine_number] += 1 if @rows[i][left][:is_mine] == true
+          end
+          if right <= @squire_max
+            @rows[i][j][:arround_mine_number] += 1 if @rows[i][right][:is_mine] == true
+          end
+          top_left = [top, left]
+          top_right = [top, right]
+          down_left = [down, left]
+          down_right = [down, right]
+          if top_left[0] >= @row_min && top_left[1] >= @squire_min
+            @rows[i][j][:arround_mine_number] += 1 if @rows[top_left[0]][top_left[1]][:is_mine] == true
+          end
+          if top_right[0] >= @row_min && top_right[1] <= @squire_max
+            @rows[i][j][:arround_mine_number] += 1 if @rows[top_right[0]][top_right[1]][:is_mine] == true
+          end
+          if down_left[0] <= @row_max && down_left[1] >= @squire_min
+            @rows[i][j][:arround_mine_number] += 1 if @rows[down_left[0]][down_left[1]][:is_mine] == true
+          end
+          if down_right[0] <= @row_max && down_right[1] <= @squire_max
+            @rows[i][j][:arround_mine_number] += 1 if @rows[down_right[0]][down_right[1]][:is_mine] == true
+          end
+        end
+      end
+    end
   end
 end
