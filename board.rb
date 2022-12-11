beb9837f008d910c7737e8e2e14a4430fe7a1516
tomoro kobori <46224993+kettle-tomorrow@users.noreply.github.com>
2022-12-11 09:17:41 +0900

    refactor: 盤面生成の初期処理をBoardクラスへ移す。しかし、テストのモックを使いやすくするため、爆弾は外から注入する

---
+++ board.rb
@@ -0,0 +1,57 @@
+class Board
+  attr_reader :squires
+
+  def initialize(mines:)
+    x1 = [{ d: "■", n: 0, m: false }, { d: "■", n: 0, m: false }, { d: "■", n: 0, m: false }]
+    x2 = [{ d: "■", n: 0, m: false }, { d: "■", n: 0, m: false }, { d: "■", n: 0, m: false }]
+    x3 = [{ d: "■", n: 0, m: false }, { d: "■", n: 0, m: false }, { d: "■", n: 0, m: false }]
+    b = [x1, x2, x3]
+
+    mine1 = mines[0]
+    mine2 = mines[1]
+
+    b[mine1[0]][mine1[1]][:m] = true
+    b[mine2[0]][mine2[1]][:m] = true
+
+    (0..2).each do |i|
+      (0..2).each do |j|
+        if b[i][j][:m] == false
+          top = i - 1
+          down = i + 1
+          left = j - 1
+          right = j + 1
+          if top >= 0
+            b[i][j][:n] += 1 if b[top][j][:m] == true
+          end
+          if down <= 2
+             b[i][j][:n] += 1 if b[down][j][:m] == true
+          end
+          if left >= 0
+            b[i][j][:n] += 1 if b[i][left][:m] == true
+          end
+          if right <= 2
+            b[i][j][:n] += 1 if b[i][right][:m] == true
+          end
+          top_left = [top, left]
+          top_right = [top, right]
+          down_left = [down, left]
+          down_right = [down, right]
+          if top_left[0] >= 0 && top_left[1] >= 0
+            b[i][j][:n] += 1 if b[top_left[0]][top_left[1]][:m] == true
+          end
+          if top_right[0] >= 0 && top_right[1] <= 2
+            b[i][j][:n] += 1 if b[top_right[0]][top_right[1]][:m] == true
+          end
+          if down_left[0] <= 2 && down_left[1] >= 0
+            b[i][j][:n] += 1 if b[down_left[0]][down_left[1]][:m] == true
+          end
+          if down_right[0] <= 2 && down_right[1] <= 2
+            b[i][j][:n] += 1 if b[down_right[0]][down_right[1]][:m] == true
+          end
+        end
+      end
+    end
+
+    @squires = b
+  end
+end
--- minesweeper.rb
+++ minesweeper.rb
@@ -1,57 +1,15 @@
+require './board'
+
 class Minesweeper
     def self.start
-        x1 = [{ d: "■", n: 0, m: false }, { d: "■", n: 0, m: false }, { d: "■", n: 0, m: false }]
-        x2 = [{ d: "■", n: 0, m: false }, { d: "■", n: 0, m: false }, { d: "■", n: 0, m: false }]
-        x3 = [{ d: "■", n: 0, m: false }, { d: "■", n: 0, m: false }, { d: "■", n: 0, m: false }]
-        b = [x1, x2, x3]
-
         mine1 = [rand(0..2), rand(0..2)]
         mine2 = [rand(0..2), rand(0..2)]
-        loop do
-            break if mine1 != mine2
-            mine2 = [rand(0..2), rand(0..2)]
-        end
-        b[mine1[0]][mine1[1]][:m] = true
-        b[mine2[0]][mine2[1]][:m] = true
 
-        (0..2).each do |i|
-            (0..2).each do |j|
-                if b[i][j][:m] == false
-                    top = i - 1
-                    down = i + 1
-                    left = j - 1
-                    right = j + 1
-                    if top >= 0
-                        b[i][j][:n] += 1 if b[top][j][:m] == true
-                    end
-                    if down <= 2
-                        b[i][j][:n] += 1 if b[down][j][:m] == true
-                    end
-                    if left >= 0
-                        b[i][j][:n] += 1 if b[i][left][:m] == true
-                    end
-                    if right <= 2
-                        b[i][j][:n] += 1 if b[i][right][:m] == true
-                    end
-                    top_left = [top, left]
-                    top_right = [top, right]
-                    down_left = [down, left]
-                    down_right = [down, right]
-                    if top_left[0] >= 0 && top_left[1] >= 0
-                        b[i][j][:n] += 1 if b[top_left[0]][top_left[1]][:m] == true
-                    end
-                    if top_right[0] >= 0 && top_right[1] <= 2
-                        b[i][j][:n] += 1 if b[top_right[0]][top_right[1]][:m] == true
-                    end
-                    if down_left[0] <= 2 && down_left[1] >= 0
-                        b[i][j][:n] += 1 if b[down_left[0]][down_left[1]][:m] == true
-                    end
-                    if down_right[0] <= 2 && down_right[1] <= 2
-                        b[i][j][:n] += 1 if b[down_right[0]][down_right[1]][:m] == true
-                    end
-                end
-            end
+        loop do
+          break if mine1 != mine2
+          mine2 = [rand(0..2), rand(0..2)]
         end
+        b = Board.new(mines: [mine1, mine2]).squires
         
         puts "マインスイーパを開始します。"
         puts "爆弾は全部で#{2}個あります。"
