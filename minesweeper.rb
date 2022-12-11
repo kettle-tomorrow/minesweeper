d3cceda03fcd20ccba6efce9c3c249718a40d442
tomoro kobori <46224993+kettle-tomorrow@users.noreply.github.com>
2022-12-11 09:47:26 +0900

    refacotr: インデントを修正

--- minesweeper.rb
+++ minesweeper.rb
@@ -2,45 +2,45 @@ require './board'
 
 class Minesweeper
     def self.start
-        mine1 = [rand(0..2), rand(0..2)]
+      mine1 = [rand(0..2), rand(0..2)]
+      mine2 = [rand(0..2), rand(0..2)]
+
+      loop do
+        break if mine1 != mine2
         mine2 = [rand(0..2), rand(0..2)]
+      end
+      board = Board.new(mines: [mine1, mine2])
 
-        loop do
-          break if mine1 != mine2
-          mine2 = [rand(0..2), rand(0..2)]
+      puts "マインスイーパを開始します。"
+      puts "爆弾は全部で#{2}個あります。"
+      puts "回答を入力してください。(例: 1,1)"
+      puts board.current_board
+
+      loop do
+        line = gets.split(',')
+        x = line[0].to_i - 1
+        y = line[1].to_i - 1
+        if board.mine?(row_number: x, squire_number: y)
+          puts "爆弾です。"
+          break 
         end
-        board = Board.new(mines: [mine1, mine2])
-        
-        puts "マインスイーパを開始します。"
-        puts "爆弾は全部で#{2}個あります。"
-        puts "回答を入力してください。(例: 1,1)"
+        board.open(row_number: x, squire_number: y)
+
+        if board.completed?
+          puts "---"
+          puts "クリアです。"
+          puts "---"
+          puts board.completed_board
+          break
+        end
+
+        puts "---"
+        puts "次の回答を入力してください。"
+        puts "---"
+
         puts board.current_board
+      end
 
-        loop do
-            line = gets.split(',')
-            x = line[0].to_i - 1
-            y = line[1].to_i - 1
-            if board.mine?(row_number: x, squire_number: y)
-                puts "爆弾です。"
-                break 
-            end
-            board.open(row_number: x, squire_number: y)
-
-            if board.completed?
-                puts "---"
-                puts "クリアです。"
-                puts "---"
-                puts board.completed_board
-                break
-            end
-
-            puts "---"
-            puts "次の回答を入力してください。"
-            puts "---"
-
-            puts board.current_board
-        end
-
-        puts "マインスイーパを終了します。"
+      puts "マインスイーパを終了します。"
     end
 end
